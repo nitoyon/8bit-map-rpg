6626f232f3d8b5b230bf3a948711ac92ac7133dd
nitoyon <nitoyon@gmail.com>
2012-04-04 04:04:26 +0900

    Added collision detection.

--- maps.js
+++ maps.js
@@ -5,13 +5,13 @@ function initialize() {
 	var _8bitTile = {
 	    getTileUrl: function(coord, zoom) {
 		return "http://mt1.google.com/vt/lyrs=8bit,m@174000000&hl=en&src=app&s=Galil&" +
-		    "z=" + zoom + "&x=" + coord.x + "&y=" + coord.y;
+		    "z=" + zoom + "&x=" + ((coord.x + 32) % 32) + "&y=" + ((coord.y + 32) % 32);
 	    },
 	    tileSize: new google.maps.Size(256, 256),
 	    isPng: true	
 	};
 	var _8bitMapType = new google.maps.ImageMapType(_8bitTile);
-	var myLatlng = new google.maps.LatLng(37.055177, 136.362305);
+	var myLatlng = new google.maps.LatLng(51.094213,2.18938);
 	var myOptions = {
 	    zoom: 5,
 	    center: myLatlng,
@@ -25,9 +25,10 @@ function initialize() {
     }
 
     function initMainCharacter(){
+	var INTERVAL = 300;
 	var direction = 2;
 	var pressing = false;
-	var x = 28, y = 32;
+	var x = 258, y = 171;
 
 	function keyHandler(evt, flag){
 	    evt = evt ? evt : window.event;
@@ -44,7 +45,7 @@ function initialize() {
 	    var new_direction = (keyCode - 38 + 4) % 4;
 	    var new_pressing = flag;
 	    
-	    if((new Date()).getTime() - prev_move_time > 500){
+	    if((new Date()).getTime() - prev_move_time > INTERVAL){
 		direction = new_direction;
 		pressing = flag;
 		update(); // update now
@@ -70,14 +71,23 @@ function initialize() {
 		var new_x = diff_x + x;
 		var new_y = diff_y + y;
 
+		console.log(new_x, new_y, map_data.substr(new_x - 10 + new_y * 512, 20));
+		if (map_data.charAt(new_x + new_y * 512) != '0'){
+		    new_x = x;
+		    new_y = y;
+		    diff_x = diff_y = 0;
+		}
+
 		map.panBy(diff_x * 16, diff_y * 16);
 		prev_move_time = (new Date()).getTime();
 		draw_direction = direction;
+		x = new_x;
+		y = new_y;
 	    }
 
 	    var index = draw_direction * 2 + (update_flag ? 1 : 0);
 	    document.getElementById("character").childNodes[0].style.top = (-index * 16) + "px";
-	    timer = setTimeout(update, 500);
+	    timer = setTimeout(update, INTERVAL);
 	}
 
 	document.onkeydown = function(e){return keyHandler(e, true);};
